3307ed15077532baf8d653ce77044c5b4493ea92
ogurecze <162505048+ogurecze@users.noreply.github.com>
2024-04-04 17:22:30 +0200

    Add files via upload

--- lab3cw3_prawie_poprawione_lepiej/src/ToppingDecorator.java
+++ lab3cw3_prawie_poprawione_lepiej/src/ToppingDecorator.java
@@ -17,26 +17,22 @@ public abstract class ToppingDecorator{
         if (pizza == null) {
             System.out.println("Nothing to remove");
         } else if (pizza.getClass().getName().equals(toRemove)) {
-            // Find the topping to be removed
             ToppingDecorator current = this;
             ToppingDecorator previous = null;
 
             while (current != null) {
                 if (current.getClass().getName().equals(toRemove)) {
                     if (previous != null) {
-                        // Update the previous topping's pizza reference to skip over the current topping
                         previous.pizza = current.pizza;
                     } else {
-                        // If the current topping is the first one in the hierarchy, update pizza reference of the current instance
                         this.pizza = current.pizza;
                     }
-                    return; // Exit the loop after removing the topping
+                    return;
                 }
                 previous = current;
                 current = current.getPizza();
             }
         } else {
-            // Recursively call removeTopping on the next topping in the hierarchy
             pizza.removeTopping(toRemove);
         }
     }
